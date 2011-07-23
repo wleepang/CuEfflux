b739e98b3f5d7fbaf72439fe902363d18cebe4ff
Lee Pang <wleepang@gmail.com>
2011-07-22 17:18:45 -0700

    More extensive changes to the simulation frame work

--- GillespieSSA.lpang.R
+++ GillespieSSA.lpang.R
@@ -1,3 +1,41 @@
+GillespieSSA:::ssa <- function (x0 = stop("undefined 'x0'"), a = stop("undefined 'a'"), 
+    nu = stop("undefined 'nu'"), parms = NULL, tf = stop("undefined 'tf'"), 
+    method = "D", simName = "", tau = 0.3, f = 10, epsilon = 0.03, 
+    nc = 10, hor = NaN, dtf = 10, nd = 100, ignoreNegativeState = TRUE, 
+    consoleInterval = 0, censusInterval = 0, verbose = FALSE, 
+    maxWallTime = Inf) 
+{
+    ssa.check.args(x0, a, nu, tf, method, tau, f, epsilon, nc, 
+        hor, dtf, nd, ignoreNegativeState, consoleInterval, censusInterval, 
+        verbose)
+    
+    method = toupper(method)
+    
+    ssa.check.method(x0, a, nu, method, tau, f)
+    if ((length(a)/dim(nu)[2] > 1) && (length(x0)/dim(nu)[1]) > 
+        1) {
+        if (method == "D") 
+            method <- "D.diag"
+        if (method == "ETL") 
+            method <- "ETL.diag"
+        if (method == "BTL") 
+            method <- "BTL.diag"
+        if (method == "OTL") 
+            method <- "OTL.diag"
+    }
+    args <- list(x0 = x0, a = a, nu = nu, parms = parms, tf = tf, 
+        method = method, tau = tau, f = f, epsilon = epsilon, 
+        nc = nc, hor = hor, dtf = dtf, nd = nd, ignoreNegativeState = ignoreNegativeState, 
+        consoleInterval = consoleInterval, censusInterval = censusInterval, 
+        verbose = verbose, simName = simName)
+    out.rxn <- ssa.run(x0, a, nu, parms, tf, method, tau, f, 
+        epsilon, nc, hor, dtf, nd, ignoreNegativeState, consoleInterval, 
+        censusInterval, verbose, maxWallTime)
+    out.summary <- ssa.terminate(args, out.rxn, tf, method, maxWallTime, 
+        verbose)
+    return(out.summary)
+}
+
 GillespieSSA:::ssa.run <- function (x0, a, nu, parms, tf, method, tau, f, epsilon, nc, 
     hor, dtf, nd, ignoreNegativeState, consoleInterval, censusInterval, 
     verbose, maxWallTime) 
@@ -75,6 +113,9 @@ GillespieSSA:::ssa.run <- function (x0, a, nu, parms, tf, method, tau, f, epsilo
                 nSuspendedTauLeaps <- nSuspendedTauLeaps + 1
                 if (!suspendedTauLeapMethod) method <- "OTL"
             }
+        }, D.ETG = {
+        		out <- ssa.d.etg(eval_a, a.s, .nu, .t, .x, partitionedSpecies)
+        		
         }, ETL = {
             out <- ssa.etl(eval_a, .nu, .tau)
         }, BTL = {
@@ -151,26 +192,55 @@ GillespieSSA:::ssa.run <- function (x0, a, nu, parms, tf, method, tau, f, epsilo
 
 # new Exact Time-dependent Gillespie algorithm as described by
 # Lu et.al Syst. Biol. (stevenage) Vol 1, No 1, 2004
-GillespieSSA:::ssa.d.etg <- function (a = stop("missing propensity vector (a)"), 
+GillespieSSA:::ssa.d.etg <- function (a   = stop("missing propensity vector (a)"), 
 																			a.s = stop("missing time dependent propensity logical vector (a.s)"), 
-																			nu = stop("missing state-change matrix (nu)")) 
+																			nu  = stop("missing state-change matrix (nu)"), 
+																	    t   = stop("missing time value"), 
+																			x   = stop("missing populations vector"),
+																			x.p = stop("missing partitioned species vector")) 
 {
   A.s = sum(a[a.s])
   A.q = sum(a[!a.s])
+  c = log(2)
   
   u1 = runif(1)
-  u2 = runif(1)
   
-  if (A.q == 0) {
-  	tau = (1/c)*log(A.s / (A.s + c*log(1 - u1)))
-  } else {
+  if (A.q != 0 && A.s != 0) {
+  	# time dependent and time independent reactions
   	alpha = A.s/A.q
   	beta = c*log(u1)/A.q
   	tau = (1/c)*lambertW(alpha*exp(alpha + beta))-alpha-beta
+  	
+  } else if (A.q == 0 && A.s != 0) {
+  	# only time dependent reactions
+  	tau = (1/c)*log(A.s / (A.s + c*log(1 - u1)))
+  	
+  } else if (A.q != 0 && A.s == 0) {
+  	# only time independent reactions (standard Gillespie)
+  	tau = -(1/A.q)*log(u1)
   }
   
-	j <- sample(seq(length(a)), size = 1, prob = a)
-  nu_j <- nu[, j]
-  tau <- -log(runif(1))/sum(a)
-  return(list(tau = tau, nu_j = nu_j))
+  cellDivision = FALSE
+  if ((t %% 1) + tau < 1) {
+  	# select reaction channel
+		j <- sample(seq(length(a)), size = 1, prob = a)
+	  nu_j <- nu[, j]
+  	# update time exactly
+  } else {
+    # update time to next cell division time
+  	tau = 1 - (t %% 1)
+  	
+  	# non-reaction based particle change
+  	# nu_j is used to divide cell populations in half
+  	nu_j = rep(0, dim(nu)[1])
+  	nu_j[x.p] = -(x[x.p] / 2)
+  	
+  	# reset volume
+  	# partition "free" species
+  	cellDivision = TRUE
+  	
+  }
+  
+  
+  return(list(tau = tau, nu_j = nu_j, cellDivision = cellDivision))
 }
