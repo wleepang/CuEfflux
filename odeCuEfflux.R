ae2cf404504ae1e2bf98982aac58713d5d3b4e2f
Lee Pang <wleepang@gmail.com>
2011-08-15 21:14:05 -0700

    the base ODE model, finished

--- odeCuEfflux.R
+++ odeCuEfflux.R
@@ -6,15 +6,21 @@ dxdt = function(time, x, parms, ...) {
   names(dxdt) = names(x)
  
   with(as.list(c(x, parms)), {
-    eval_a <- rep(0, length(a))
-    parse_a <- parse(text = a)
+    eval_a = rep(0, length(a))
+    names(eval_a) = names(a)
+    parse_a = parse(text = a)
     for (.i in 1:length(parse_a)) {
-      eval_a[.i] <- eval(parse_a[.i])
+      eval_a[.i] = eval(parse_a[.i])
     }
     if (any(eval_a < 0)) {
       warning("negative propensities coerced to zero")
       eval_a[any(eval_a)] = 0
     }
+    
+    # cell division is a special case that allows a negative propensity:
+    # if (V >= V.max) {
+    #  eval_a['V growth'] = -(V-V.min)
+    # }
     
     dxdt = drop(sign(nu) %*% eval_a)
     return(list(dxdt))
