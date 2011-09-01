34f150d848a768cb3ba4c215f2b3631f40d662cf
Lee Pang <wleepang@gmail.com>
2011-08-31 18:37:51 -0700

    strain comparison code

--- CuEfflux_rxnDef.R
+++ CuEfflux_rxnDef.R
@@ -106,12 +106,12 @@ rxn[['Mgfp transcription']] = list(
 	nu = setStoic(x, c('P1179.Cu.Dgfp', 'P1179.Cu', 'Dgfp', 'Mgfp'), c(-1, +1, +1, +1)))
 
 rxn[['OE0702 transcription']] = list(
-	a = '0.646571*OE0702', # in order to get OE to be like expt. divide by factor of 100
+	a = '0.646571*OE0702/100', # in order to get OE to be like expt. divide by factor of 100
 	a.s = FALSE, 
 	nu = setStoic(x, 'M0702', +1))
 
 rxn[['OE2581 transcription']] = list(
-	a = '0.424167*OE2581', # in order to get OE to be like expt. divide by factor of 100
+	a = '0.424167*OE2581/100', # in order to get OE to be like expt. divide by factor of 100
 	a.s = FALSE, 
 	nu = setStoic(x, 'M2581', +1))
 
--- odeCuEfflux.R
+++ odeCuEfflux.R
@@ -4,7 +4,11 @@ source('CuEfflux_Func.R')
 
 # set global values
 source('CuEfflux_GlobalParams.R')
-isOE=T
+isKO0702 = F
+isKO2581 = F
+isOE0702 = F
+isOE2581 = F
+
 # initialize set model parameters and initial state
 source('CuEfflux_Init.R')
 
@@ -24,14 +28,14 @@ t.abs = out[,'time'] / 60 # sim time is in seconds, convert to minutes
 
 plot(t.abs, .T('Cu', out)/out[,'V'], type='l')
 
-rna = cbind(out[, 'M0700'], out[, 'M0702'], out[, 'Mgfp'])/out[,'V']
-matplot(t.abs, rna, type='l', col=c('blue', 'red', 'green'), lwd=3, lty=1, ann=F)
-legend('topright', c('0700', 'Chaperone', 'gfp'), cex=0.8, lwd=3, col=c('blue', 'red', 'green'), bty='n')
+rna = cbind(out[, 'M0700'], out[, 'M0702'], out[, 'M2581'], out[, 'Mgfp'])/out[,'V']
+matplot(t.abs, rna, type='l', col=c('black','blue', 'red', 'green'), lwd=3, lty=1, ann=F)
+legend('topright', c('0700', '0702', '2581', 'gfp'), cex=0.8, lwd=3, col=c('black', 'blue', 'red', 'green'), bty='n')
 title(xlab='Time (min)', ylab='mRNA Conc')
 
-prot = cbind(.T('P0700', out), .T('P0702', out)/10, out[, 'Pgfp'])/out[,'V']
-matplot(t.abs, prot, type='l', col=c('blue', 'red', 'green'), lwd=3, lty=1, ann=F)
-legend('topright', c('0700', 'Chaperone (x10)', 'GFP'), cex=0.8, lwd=3, col=c('blue', 'red', 'green'), bty='n')
+prot = cbind(.T('P0700', out), .T('P0702', out)/10, .T('P2581', out)/10, out[, 'Pgfp'])/out[,'V']
+matplot(t.abs, prot, type='l', col=c('black', 'blue', 'red', 'green'), lwd=3, lty=1, ann=F)
+legend('topright', c('0700', '0702 (x10)', '2581 (x10)', 'GFP'), cex=0.8, lwd=3, col=c('black', 'blue', 'red', 'green'), bty='n')
 title(xlab='Time (min)', ylab='Protein Conc')
 
 #t.abs[which(rna[,1] == max(rna[,1]))]
